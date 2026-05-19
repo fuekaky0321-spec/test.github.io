9f5557640bcd406a1bb17a264d494d9165875e26
fuekaky0321-spec <fuekaky0321@gmail.com>
2026-05-19 11:40:44 +0900

    Add files via upload

---
+++ cast.java
@@ -0,0 +1,6 @@
+const CAST_DICT = {
+  "MAAYA": "https://www.canva.com/design/DAHKEvpK2hk/pFVKh--tI0O1TqFShAN6tA/view",
+  "NAKO": "https://www.canva.com/design/DAHKEj4Qwf0/BpbRYJCz2i66K8OdyhcLcA/view",
+  "HONO": "https://www.canva.com/design/DAHKEhBRJws/HTyusJhLG7ZV0EdGPkORGg/view",
+  "YU": "https://www.canva.com/design/DAHKEqQ7D1U/q9OnBGdS0_5P-hdb5xSJKw/view",
+};
